321caed99b70b1041eaff3a8b419e9a468ec77ca
mdqleet <fabian23gamer@gmail.com>
2025-08-24 18:42:41 +0300

    Translated the comments in English

--- tests/logintest.spec.js
+++ tests/logintest.spec.js
@@ -1,48 +1,48 @@
 import {test , expect} from 'playwright/test'
 
 test('Login_Success', async ({ page }) => {
-    // Deschide pagina
+    // Open the page
     await page.goto ('https://practicetestautomation.com/practice-test-login/')
-    // Input pentru field-ul 'username'
+    // Fill in username
     await page.fill ('#username', 'student');
-    // Input pentru field-ul 'password
+    // Fill in password
     await page.fill ('#password', 'Password123');
     // Click 'submit'
     await page.click ('text=Submit');
-    // Verifica link-ul dupa submit
+    // Verify the link after submit
     await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
-    // Verifica mesajul de felcicitari dupa connect
+    // Check congratulations message
     await expect(page.getByText('Congratulations student. You successfully logged in!')).toBeVisible();
-    // Click Log out dupa conectare
+    // Log out after connection
     await page.click ('text=Log out')
 })
 
 test('Login_Fail_User', async ({ page }) => {
-    // Deschide pagina
+    // Open the page
     await page.goto ('https://practicetestautomation.com/practice-test-login/')
-    // Input gresit pt field-ul 'username'
+    // Wrong input for username
     await page.fill ('#username', 'incorrectUser');
-    // Input corect pt field-ul 'password'
+    // Correct input for password
     await page.fill ('#password', 'Password123');
     // Click 'submit'
     await page.click ('text=Submit')
-    // Verifica mesajul de fail (daca apare)
+    // Check if the error message appears
     await expect(page.locator('#error')).toBeVisible();
-    // Verifica mesajul de fail (daca e corect)
+    // Check if the error message is correct
     await expect(page.locator('#error')).toHaveText('Your username is invalid!');
 })
 
 test('Login_Fail_Password', async ({ page }) => {
-    // Deschide pagina
+    // Open the page
     await page.goto ('https://practicetestautomation.com/practice-test-login/')
-    // Input corect pt field-ul 'username'
+    // Correct input for username
     await page.fill ('#username', 'student');
-    // Input gresit pt field-ul 'password'
+    // Wrong input for password
     await page.fill ('#password', 'incorrectPassword');
     // Click 'submit'
     await page.click ('text=Submit')
-    // Verifica mesajul de fail (daca apare)
+    // Check if the error message appears
     await expect(page.locator('#error')).toBeVisible();
-    // Verifica mesajul de fail (daca e corect)
+    // Check if the error message is correct
     await expect(page.locator('#error')).toHaveText('Your password is invalid!');
 })
