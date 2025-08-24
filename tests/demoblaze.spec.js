05c80a8c616de3c12c8e8833339393bbe1c51775
mdqleet <fabian23gamer@gmail.com>
2025-08-24 18:31:21 +0300

    - Deleted the example file under the tests folder - Added 2 new test scenarios for Demoblaze - Changed the comments language in English

--- tests/demoblaze.spec.js
+++ tests/demoblaze.spec.js
@@ -1,18 +1,17 @@
-// Importăm funcțiile principale din Playwright Test
+// Importing functions from Playwright
 const { test, expect } = require('@playwright/test');
 
-// Testul 1: verificăm titlul homepage-ului
-test('Homepage title should contain STORE', async ({ page }) => {
-  // Navigăm către site-ul DemoBlaze
+// T1 - Verify the title of the page
+test('Homepage_Title', async ({ page }) => {
+
   await page.goto('https://demoblaze.com/index.html');
 
-  // Verificăm că titlul paginii conține "STORE"
   await expect(page).toHaveTitle(/STORE/);
 });
 
-// Testul 2: dam click pe categoria phones
-test('Click on the phones category', async ({ page }) => {
- //Selectam categoria
+// T2 - Click the 'Phones' category
+test('Click_Phones', async ({ page }) => {
+ 
   await page.goto('https://demoblaze.com/index.html');
 
   await page.click('text=Phones');
@@ -20,18 +19,70 @@ test('Click on the phones category', async ({ page }) => {
   await page.waitForTimeout(10000);
 });
 
-// Testul 3: dăm click pe primul produs și verificăm detaliile
-test('Click on first product should open product details', async ({ page }) => {
-  // Navigăm către site-ul DemoBlaze
+// T3 - Click on the first product and locate details
+test('Click_Product', async ({ page }) => {
+
   await page.goto('https://demoblaze.com/index.html');
 
   await page.click('text=Phones');
 
-  // Dăm click pe primul produs (selector CSS pentru link-ul produsului)
   await page.click('a.hrefch');
 
-  // Selectăm containerul detaliilor produsului și verificăm că este vizibil
   const productDetail = page.locator('#tbodyid');
   await expect(productDetail).toBeVisible();
 
+});
+
+// T4 - Log In/Log Out
+test('Accounts', async ({ page }) => {
+
+  await page.goto('https://demoblaze.com/index.html')
+
+  await page.click('id=login2')
+
+  await page.fill('id=loginusername', 'pavanol')
+
+  await page.fill('id=loginpassword', 'test@123')
+
+  await page.getByRole('button', { name: 'Log in' }).click();
+
+  await page.click('id=logout2')
+})
+
+// T4 - Add products in cart and order
+test('Products', async ({ page }) => {
+
+  await page.goto ('https://demoblaze.com/index.html')
+
+  await page.getByRole('link', { name: 'Monitors' }).click()
+  await page.getByRole('link', { name: 'Apple monitor 24' }).click();
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.once('dialog', async dialog => {
+  console.log(dialog.message());  
+  await dialog.accept();
+  });
+  await page.goBack();
+  await page.goBack();
+
+
+  await page.getByRole('link', { name: 'Nexus 6' }).click();
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await page.once('dialog', async dialog => {
+  console.log(dialog.message());  
+  await dialog.accept();
+  });
+  await page.goBack();
+  await page.goBack();
+
+  await page.getByRole('link', { name: 'Cart'}).click();
+  await page.getByRole('button', { name: 'Place Order'}).click();
+
+  await page.fill('#name', 'Andrew Smith');
+  await page.fill('#country', 'Alaska');
+  await page.fill('#city', 'Alaska')
+  await page.fill('#card', '4444 4444 4444 4441')
+  await page.fill('#month', 'March')
+  await page.fill('#year', '2027')
+  await page.getByRole('button', { name: 'Purchase'}).click();
+  await expect(page.getByText('Thank you for your purcha!')).toBeVisible();
 });
