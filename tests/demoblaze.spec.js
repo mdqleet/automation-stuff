d12743c79024cb8413b4a127ff132b7f64e873d0
mdqleet <fabian23gamer@gmail.com>
2025-08-24 18:47:01 +0300

    Fixed the string that caused the run to fail

--- tests/demoblaze.spec.js
+++ tests/demoblaze.spec.js
@@ -3,49 +3,32 @@ const { test, expect } = require('@playwright/test');
 
 // T1 - Verify the title of the page
 test('Homepage_Title', async ({ page }) => {
-
   await page.goto('https://demoblaze.com/index.html');
-
   await expect(page).toHaveTitle(/STORE/);
 });
 
 // T2 - Click the 'Phones' category
 test('Click_Phones', async ({ page }) => {
- 
   await page.goto('https://demoblaze.com/index.html');
-
   await page.click('text=Phones');
-
-  await page.waitForTimeout(10000);
 });
 
 // T3 - Click on the first product and locate details
 test('Click_Product', async ({ page }) => {
-
   await page.goto('https://demoblaze.com/index.html');
-
   await page.click('text=Phones');
-
   await page.click('a.hrefch');
-
   const productDetail = page.locator('#tbodyid');
   await expect(productDetail).toBeVisible();
-
 });
 
 // T4 - Log In/Log Out
 test('Accounts', async ({ page }) => {
-
   await page.goto('https://demoblaze.com/index.html')
-
   await page.click('id=login2')
-
   await page.fill('id=loginusername', 'pavanol')
-
   await page.fill('id=loginpassword', 'test@123')
-
   await page.getByRole('button', { name: 'Log in' }).click();
-
   await page.click('id=logout2')
 })
 
@@ -84,5 +67,5 @@ test('Products', async ({ page }) => {
   await page.fill('#month', 'March')
   await page.fill('#year', '2027')
   await page.getByRole('button', { name: 'Purchase'}).click();
-  await expect(page.getByText('Thank you for your purcha!')).toBeVisible();
+  await expect(page.getByText('Thank you for your purchase!')).toBeVisible();
 });
